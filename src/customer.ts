4354fedafd42e181a5091e14fbd34e218a78fb5a
William Trevisan <williamtrev@hotmail.com>
2022-05-31 16:18:52 -0300

    feat: create a customer entity

--- src/customer.ts
+++ src/customer.ts
@@ -1,11 +1,37 @@
 class Customer {
   _id: string;
-  _name: string;
-  _address: string;
+  _name: string = "";
+  _address: string = "";
+  _active: boolean = false;
 
-  constructor (id: string, name: string, address: string) {
+  constructor (id: string, name: string) {
     this._id = id;
     this._name = name;
-    this._address = address;
+    
+    this.validate();
+  }
+
+  changeName(name: string): void {
+    this._name = name;
+
+    this.validate();
+  }
+
+  activate() {
+    if (!this._address) {
+      throw new Error("Address is mandatory to activate a customer.");
+    }
+
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+
+  validate() {
+    if (!this._id) throw new Error("Id is required.");
+
+    if (!this._name) throw new Error("Name is required.");
   }
 }
