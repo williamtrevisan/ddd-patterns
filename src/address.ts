e41d7bfeada672fc884fdcdcf52b43847ac00cd1
William Trevisan <williamtrev@hotmail.com>
2022-05-31 16:23:28 -0300

    feat: created a value object address

---
+++ src/address.ts
@@ -0,0 +1,25 @@
+class Address {
+  _street: string = "";
+  _number: number = 0;
+  _zip: string = "";
+  _city: string = "";
+
+  constructor (street: string, number: number, zip: string, city: string) {
+    this._street = street;
+    this._number = number;
+    this._zip = zip;
+    this._city = city;
+
+    this.validate();
+  }
+
+  validate() {
+    if (!this._street) throw new Error("Street is required.");
+    
+    if (!this._number) throw new Error("Number is required.");
+    
+    if (!this._zip) throw new Error("Zip is required.");
+    
+    if (!this._city) throw new Error("City is required.");
+  }
+}
