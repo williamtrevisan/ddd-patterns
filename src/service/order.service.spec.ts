fbdbb3cbb0e24088387c216e2f2e5dd7d9065929
William Trevisan <williamtrev@hotmail.com>
2022-06-01 13:26:59 -0300

    test: created a test for place an order into a order service

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,19 @@
+import { Customer } from "../entity/customer";
 import { Order } from "../entity/order";
 import { OrderItem } from "../entity/order_item";
 import { OrderService } from "./order.service";
 
 describe("Order service unit tests", () => {
+  it("should place an order", () => {
+    const customer = new Customer("customerId", "Customer name");
+    const orderItem = new OrderItem("itemId", "Item name", 10, "productId", 1);
+
+    const order = OrderService.placeOrder(customer, [orderItem]);
+
+    expect(customer.rewardPoints).toBe(5);
+    expect(order.total()).toBe(10);
+  });
+
   it("should get total of all orders", () => {
     const orderItem = new OrderItem("itemId", "Item name", 100, "productId", 1);
     const orderItem2 = new OrderItem("itemId2", "Item name 2", 200, "productId", 2);
