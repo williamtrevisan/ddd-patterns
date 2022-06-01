7144245712f6314f3249167e2e0aa8064a544587
William Trevisan <williamtrev@hotmail.com>
2022-06-01 13:19:39 -0300

    test: created a test for order service

---
+++ src/service/order.service.spec.ts
@@ -0,0 +1,15 @@
+import { Order } from "../entity/order";
+import { OrderItem } from "../entity/order_item";
+
+describe("Order service unit tests", () => {
+  it("should get total of all orders", () => {
+    const orderItem = new OrderItem("itemId", "Item name", 100, "productId", 1);
+    const orderItem2 = new OrderItem("itemId2", "Item name 2", 200, "productId", 2);
+    const order = new Order("orderId", "customerId", [orderItem]);
+    const order2 = new Order("orderId2", "customerId", [orderItem2]);
+
+    const total = OrderService.total([order, order2]);
+
+    expect(total).toBe(500);
+  });
+});
