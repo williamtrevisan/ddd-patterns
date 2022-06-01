ce63b8ef4dcbb59b733a21cbb8fb342a12a62896
William Trevisan <williamtrev@hotmail.com>
2022-06-01 14:52:17 -0300

    feat: created a repository interface

---
+++ src/domain/repository/repository.interface.ts
@@ -0,0 +1,8 @@
+interface RepositoryInterface<T> {
+  create(entity: T): Promise<void>;
+  update(entity: T): Promise<void>;
+  find(id: string): Promise<T>;
+  findAll(): Promise<T[]>;
+}
+
+export { RepositoryInterface };
