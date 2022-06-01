4b346597a5295ef869eb34629ab5eea231b87bb6
William Trevisan <williamtrev@hotmail.com>
2022-06-01 08:36:03 -0300

    feat: created total method into a order entity

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,14 +1,21 @@
+/* eslint-disable no-underscore-dangle */
 import { OrderItem } from "./order_item";
 
 class Order {
-  id: string;
-  customerId: string;
-  items: OrderItem[];
+  private _id: string;
+  private _customerId: string;
+  private _items: OrderItem[];
+  private _total: number;
 
   constructor(id: string, customerId: string, items: OrderItem[]) {
-    this.id = id;
-    this.customerId = customerId;
-    this.items = items;
+    this._id = id;
+    this._customerId = customerId;
+    this._items = items;
+    this._total = this.total();
+  }
+
+  total(): number {
+    return this._items.reduce((acc, item) => acc + item.price, 0);
   }
 }
 
