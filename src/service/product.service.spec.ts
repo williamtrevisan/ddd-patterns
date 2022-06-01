627001e198d1dd50f353560997589fb356b1d18d
William Trevisan <williamtrev@hotmail.com>
2022-06-01 13:04:38 -0300

    test: created a test for product service

---
+++ src/service/product.service.spec.ts
@@ -0,0 +1,13 @@
+import { Product } from "entity/product";
+
+describe("Product service unit tests", () => {
+  it("should change the prices of all products", () => {
+    const product_one = new Product("productId", "Product name", 10);
+    const product_two = new Product("productId", "Product name", 10);
+
+    ProductService.increasePrice([product_one, product_two], 100);
+
+    expect(product_one.price).toBe(20);
+    expect(product_two.price).toBe(40);
+  });
+});
