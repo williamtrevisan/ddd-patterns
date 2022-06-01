ff3c123721aa63d6279d8c708838265b77ca3508
William Trevisan <williamtrev@hotmail.com>
2022-06-01 09:20:54 -0300

    feat: created a getter for price into a order_item entity

--- src/entity/order_item.ts
+++ src/entity/order_item.ts
@@ -1,12 +1,27 @@
+/* eslint-disable no-underscore-dangle */
 class OrderItem {
-  id: string;
-  name: string;
-  price: number;
+  private _id: string;
+  private _productId: string;
+  private _name: string;
+  private _price: number;
+  private _quantity: number;
 
-  constructor(id: string, name: string, price: number) {
-    this.id = id;
-    this.name = name;
-    this.price = price;
+  constructor(
+    id: string,
+    name: string,
+    price: number,
+    productId: string,
+    quantity: number
+  ) {
+    this._id = id;
+    this._name = name;
+    this._price = price;
+    this._productId = productId;
+    this._quantity = quantity;
+  }
+
+  get price(): number {
+    return this._price * this._quantity;
   }
 }
 
