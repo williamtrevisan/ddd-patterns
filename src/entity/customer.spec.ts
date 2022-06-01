7fb7654c751c9a42d64995d6d82de8dbefab91e4
William Trevisan <williamtrev@hotmail.com>
2022-06-01 14:47:15 -0300

    feat: created a new test for add reward points into a customer tests

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -53,4 +53,15 @@ describe("Customer unit tests", () => {
 
     expect(customer.isActive()).toBe(false);
   });
+
+  it("should add reward points", () => {
+    const customer = new Customer("customerId", "Customer name");
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(20);
+  });
 });
