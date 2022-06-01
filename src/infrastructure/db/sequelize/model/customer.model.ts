09d700d175f70abbfba9d7b2cd1babf4bb496908
William Trevisan <williamtrev@hotmail.com>
2022-06-01 15:57:24 -0300

    feat: created a CustomerRepositoryInterface and a CustomerModel

---
+++ src/infrastructure/db/sequelize/model/customer.model.ts
@@ -0,0 +1,31 @@
+import { Column, Model, PrimaryKey, Table } from "sequelize-typescript";
+
+@Table({
+  tableName: "customers",
+  timestamps: false,
+})
+class CustomerModel extends Model {
+  @PrimaryKey
+  @Column
+  declare id: string;
+
+  @Column({ allowNull: false })
+  declare name: string;
+
+  @Column({ allowNull: false })
+  declare street: string;
+
+  @Column({ allowNull: false })
+  declare number: number;
+
+  @Column({ allowNull: false })
+  declare zipcode: string;
+
+  @Column({ allowNull: false })
+  declare active: boolean;
+
+  @Column({ allowNull: false })
+  declare rewardPoints: number;
+}
+
+export { CustomerModel };
