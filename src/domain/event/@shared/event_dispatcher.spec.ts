05d05cb45f08dcb5ab02cbb79502ecce8472ffef
William Trevisan <williamtrev@hotmail.com>
2022-06-02 20:50:42 -0300

    test: created a test for notify event handlers into a event dispatcher tests

--- src/domain/event/@shared/event_dispatcher.spec.ts
+++ src/domain/event/@shared/event_dispatcher.spec.ts
@@ -1,4 +1,5 @@
 import { SendEmailWhenProductIsCreatedHandler } from "../product/handler/send_email_when_product_is_created.handler";
+import { ProductCreatedEvent } from "../product/product_created.event";
 import { EventDispatcher } from "./event-dispatcher";
 
 describe("Domain events tests", () => {
@@ -38,5 +39,25 @@ describe("Domain events tests", () => {
     eventDispatcher.unregisterAll();
 
     expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined();
+  });
+
+  it("should notify all event handlers", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+
+    eventDispatcher.register("ProductCreatedEvent", eventHandler);
+
+    expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toMatchObject(eventHandler);
+
+    const productCreatedEvent = new ProductCreatedEvent({
+      name: "Product name",
+      description: "Product description",
+      price: 10.0
+    });
+
+    eventDispatcher.notify(productCreatedEvent);
+
+    expect(spyEventHandler).toHaveBeenCalled();
   });
 });
