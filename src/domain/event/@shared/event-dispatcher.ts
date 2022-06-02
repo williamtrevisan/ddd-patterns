ea7c45a4ae1efae190442a77f76085c804cfd9cc
William Trevisan <williamtrev@hotmail.com>
2022-06-02 20:28:18 -0300

    feat: implemented register method into a eventDispatcher

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -3,12 +3,22 @@ import { EventDispatcherInterface } from "./event_dispatcher.interface";
 import { EventHandlerInterface } from "./event_handler.interface";
 
 class EventDispatcher implements EventDispatcherInterface {
+  private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+  get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+    return this.eventHandlers;
+  }
+
   notify(event: EventInterface): void {
     throw new Error("Method not implemented.");
   }
   
-  register(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
-    throw new Error("Method not implemented.");
+  register(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+    }
+
+    this.eventHandlers[eventName].push(eventHandler);
   }
   
   unregister(eventName: string, eventHandler: EventDispatcherInterface): void {
--- src/domain/event/@shared/event_dispatcher.spec.ts
+++ src/domain/event/@shared/event_dispatcher.spec.ts
@@ -1,3 +1,6 @@
+import { SendEmailWhenProductIsCreatedHandler } from "../product/handler/send_email_when_product_is_created.handler";
+import { EventDispatcher } from "./event-dispatcher";
+
 describe("Domain events tests", () => {
   it("should register an event handler", () => {
     const eventDispatcher = new EventDispatcher();
