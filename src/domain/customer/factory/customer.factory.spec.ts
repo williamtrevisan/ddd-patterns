bd7a6a85d08aba7889d4dee6b93771e535706a5f
William Trevisan <williamtrev@hotmail.com>
2022-06-03 14:24:50 -0300

    test: created a first test into a customer factory tests

---
+++ src/domain/customer/factory/customer.factory.spec.ts
@@ -0,0 +1,9 @@
+describe("Customer factory unit test", () => {
+  it("should create a customer", () => {
+    const customer = CustomerFactory.create("William");
+
+    expect(customer.id).toBeDefined();
+    expect(customer.name).toBe("William");
+    expect(customer.address).toBeUndefined();
+  });
+});
