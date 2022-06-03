3af342e7517f0ed99acde7b11c913c51fc3577da
William Trevisan <williamtrev@hotmail.com>
2022-06-02 21:10:21 -0300

    feat: created a two SendConsoleLogHandlers for CustomerCreatedEvent

---
+++ src/domain/event/customer/handler/send_console_log_1_when_customer_is_created.ts
@@ -0,0 +1,10 @@
+import { EventHandlerInterface } from "../../@shared/event_handler.interface";
+import { CustomerCreatedEvent } from "../custumer_created.event";
+
+class SendConsoleLog1WhenCustomerIsCreatedHandler implements EventHandlerInterface<CustomerCreatedEvent> {
+  handle(event: CustomerCreatedEvent): void {
+    console.log("Esse é o primeiro console.log do evento: CustomerCreated.");
+  }
+}
+
+export { SendConsoleLog1WhenCustomerIsCreatedHandler };
---
+++ src/domain/event/customer/handler/send_console_log_2_when_customer_is_created.ts
@@ -0,0 +1,10 @@
+import { EventHandlerInterface } from "../../@shared/event_handler.interface";
+import { CustomerCreatedEvent } from "../custumer_created.event";
+
+class SendConsoleLog2WhenCustomerIsCreatedHandler implements EventHandlerInterface<CustomerCreatedEvent> {
+  handle(event: CustomerCreatedEvent): void {
+    console.log("Esse é o segundo console.log do evento: CustomerCreated.");
+  }
+}
+
+export { SendConsoleLog2WhenCustomerIsCreatedHandler };
