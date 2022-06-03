196e14878868b247907d7a358de55f53f2a8adc4
William Trevisan <williamtrev@hotmail.com>
2022-06-02 21:17:50 -0300

    test: created a test for notify event handlers for customer created into a event dispatcher tests

--- src/domain/event/@shared/event_dispatcher.spec.ts
+++ src/domain/event/@shared/event_dispatcher.spec.ts
@@ -1,3 +1,8 @@
+import { Address } from "../../entity/address";
+import { Customer } from "../../entity/customer";
+import { CustomerCreatedEvent } from "../customer/custumer_created.event";
+import { SendConsoleLog1WhenCustomerIsCreatedHandler } from "../customer/handler/send_console_log_1_when_customer_is_created";
+import { SendConsoleLog2WhenCustomerIsCreatedHandler } from "../customer/handler/send_console_log_2_when_customer_is_created";
 import { SendEmailWhenProductIsCreatedHandler } from "../product/handler/send_email_when_product_is_created.handler";
 import { ProductCreatedEvent } from "../product/product_created.event";
 import { EventDispatcher } from "./event-dispatcher";
@@ -59,5 +64,31 @@ describe("Domain events tests", () => {
     eventDispatcher.notify(productCreatedEvent);
 
     expect(spyEventHandler).toHaveBeenCalled();
+  });
+
+  it("should notify all event handlers for CustumerCreatedEvent", () => {
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler1 = new SendConsoleLog1WhenCustomerIsCreatedHandler();
+    const eventHandler2 = new SendConsoleLog2WhenCustomerIsCreatedHandler();
+    const spyEventHandler1 = jest.spyOn(eventHandler1, "handle");
+    const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
+    
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][0]).toMatchObject(eventHandler1);
+    expect(eventDispatcher.getEventHandlers["CustomerCreatedEvent"][1]).toMatchObject(eventHandler2);
+
+    const customer = new Customer("customerId", "Customer name");
+    const address = new Address("Street name", 1, "Zipcode", "City name");
+    customer.changeAddress(address)
+    customer.activate();
+
+    const customerCreatedEvent = new CustomerCreatedEvent(customer);
+
+    eventDispatcher.notify(customerCreatedEvent);
+
+    expect(spyEventHandler1).toHaveBeenCalled();
+    expect(spyEventHandler2).toHaveBeenCalled();
   });
 });
