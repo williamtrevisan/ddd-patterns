44e900c6afbc263fcfbf66a0f0afae1144fc59b1
William Trevisan <williamtrev@hotmail.com>
2022-06-03 14:38:11 -0300

    feat: created a createWithAddress method into a productFactory

--- src/domain/customer/factory/customer.factory.ts
+++ src/domain/customer/factory/customer.factory.ts
@@ -1,10 +1,18 @@
 import { v4 as uuidV4 } from "uuid";
 
 import { Customer } from "../entity/customer";
+import { Address } from "../value_object/address";
 
 class CustomerFactory {
   public static create(name: string): Customer {
     return new Customer(uuidV4(), name);
+  }
+
+  public static createWithAddress(name: string, address: Address): Customer {
+    const customer = new Customer(uuidV4(), name);
+    customer.changeAddress(address);
+
+    return customer;
   }
 }
 
