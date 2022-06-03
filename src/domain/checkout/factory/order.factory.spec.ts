a61a740b03d9cbb388f3aba4186773d065b4bf0c
William Trevisan <williamtrev@hotmail.com>
2022-06-03 15:49:59 -0300

    test: created a first test into a order factory tests

---
+++ src/domain/checkout/factory/order.factory.spec.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidV4 } from "uuid";
+
+describe("Order factory unit tests", () => {
+  it("should create an order", () => {
+    const orderProps = {
+      id: uuidV4(),
+      customerId: uuidV4(),
+      items: [
+        {
+          id: uuidV4(),
+          name: "Product name",
+          productId: uuidV4(),
+          quantity: 2,
+          price: 175,
+        },
+      ],
+    };
+
+    const order = OrderFactory.create(orderProps);
+
+    expect(order.id).toEqual(orderProps.id);
+    expect(order.customerId).toEqual(orderProps.customerId);
+    expect(order.items.length).toBe(1);
+  });
+});
